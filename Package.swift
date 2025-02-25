cc16ede1ce44e94cf47dcc8fdc4379b5e6dbc61f
Mrteller <39244601+Mrteller@users.noreply.github.com>
2025-02-25 18:40:35 +0300

    CHANGE: shaps80 Logging

--- Package.swift
+++ Package.swift
@@ -5,14 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "LoggingOSLog",
-    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6),
+    ],
     products: [
         .library(
             name: "LoggingOSLog",
             targets: ["LoggingOSLog"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/shaps80/Logging.git", from: "1.0.0"),
     ],
     targets: [
         .target(
